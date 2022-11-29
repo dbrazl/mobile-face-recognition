21f1f2103993d89292cf6cfe576673a0d6187c39
Daniel Braz <danielbrazlopes16@gmail.com>
2022-11-29 12:42:17 -0300

    feat: create logic to navigate to transfer

--- mobile/src/pages/AreaPix/index.tsx
+++ mobile/src/pages/AreaPix/index.tsx
@@ -1,41 +1,51 @@
 import React from 'react';
 import {StatusBar} from 'react-native';
 import transper from '../../assets/images/transaction.png';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {useNavigation} from '@react-navigation/native';
 
 import * as C from './styles';
 
 const AreaPix: React.FC = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
+
   const functions = [
     {
       icon: <C.Icon source={transper} />,
       label: 'Transferir',
+      onPress: () => navigation.navigate('Transfer'),
     },
     {
       icon: '',
       label: 'Função 2',
+      onPress: () => {},
     },
     {
       icon: '',
       label: 'Função 3',
+      onPress: () => {},
     },
     {
       icon: '',
       label: 'Função 4',
+      onPress: () => {},
     },
     {
       icon: '',
       label: 'Função 5',
+      onPress: () => {},
     },
     {
       icon: '',
       label: 'Função 6',
+      onPress: () => {},
     },
   ];
 
   const renderItem = ({item}) => {
     return (
       <C.Column>
-        <C.Item>{item.icon}</C.Item>
+        <C.Item onPress={item.onPress}>{item.icon}</C.Item>
         <C.LabelItem>{item.label}</C.LabelItem>
       </C.Column>
     );
